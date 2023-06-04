db523cd2fa18a9b7eb0f137cc1306a57454d49df
Esteban Quintero <aeqyuntero@gmail.com>
2023-06-04 13:18:18 -0500

    Adición de confirmación al cerrar sesion

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar',
@@ -12,10 +13,19 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {}
 
   cerrarSesion() {
-    localStorage.removeItem('usuario');
-    localStorage.removeItem('token');
+    Swal.fire({
+      title: 'Confirmar',
+      icon: 'question',
+      text: '¿Estás seguro de que deseas cerrar sesión?',
+      showCancelButton: true,
+    }).then((confirmar) => {
+      if (confirmar.isConfirmed) {
+        localStorage.removeItem('usuario');
+        localStorage.removeItem('token');
 
-    window.location.reload();
+        window.location.reload();
+      }
+    });
   }
 
   get isLogged(): boolean {
